14348497e3715a7d9f3b9e32030f6ed7e780795c
nathan <nath.sathanandan@gmail.com>
2022-03-30 13:05:42 +0100

    created simple birthday reminder app

--- react-projects/birthday-reminder/src/App.js
+++ react-projects/birthday-reminder/src/App.js
@@ -3,7 +3,23 @@ import data from './data';
 import List from './List';
 
 function App() {
-  return <h2>Reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
